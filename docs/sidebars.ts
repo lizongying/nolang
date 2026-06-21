ff84e0b550c6d36f907e572c7557df73d9e87e9e
李宗英 <lizongying@msn.com>
2026-06-21 18:25:54 +0800

    docs(keywords): Fix the symbol display escaping issue in the documentation
    Wrap symbols like <, <=, <- in the documentation with the <code> tag and escape them to avoid rendering anomalies.

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -1,22 +1,22 @@
-import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'intro',
+    "intro",
     {
-      type: 'category',
-      label: '語法',
-      items: ['lang/syntax'],
+      type: "category",
+      label: "語法",
+      items: ["lang/syntax", "lang/str"],
     },
     {
-      type: 'category',
-      label: '關鍵字',
-      items: ['lang/keywords'],
+      type: "category",
+      label: "運算符",
+      items: ["lang/symbol"],
     },
     {
-      type: 'category',
-      label: '標準庫',
-      items: ['std/overview'],
+      type: "category",
+      label: "標準庫",
+      items: ["std/overview"],
     },
   ],
 };
